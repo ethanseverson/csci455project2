92755b52b5d61d98221a3e2bdeba152e09eb795f
Ethan Severson <ethan.severson@ndsu.edu>
2023-10-22 21:48:17 -0500

    Completed project specifications, missing documentation

--- FundraiserClient/src/FundraiserClient/Main.java
+++ FundraiserClient/src/FundraiserClient/Main.java
@@ -1,25 +1,80 @@
 package FundraiserClient;
+
 import java.io.*;
 import java.net.*;
+import java.util.regex.Pattern;
 
 public class Main {
+
+    // Validate IPv4 address using regex
+    // Regex from https://stackoverflow.com/questions/5284147/validating-ipv4-addresses-with-regexp
+    public static boolean isValidIP(String ip) {
+        String regex = "^(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\\.){3}"
+                     + "(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$";
+        return Pattern.matches(regex, ip);
+    }
 
     public static void main(String[] args) throws Exception {
         String sentence;
         String response;
+        String serverIP;
+        int serverPort;
 
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("localhost", 6900); // Replace with your server's address and port
+        Socket clientSocket = null;
+
+        // Prompt and validate server IP and port
+        while (true) {
+            System.out.print(">> Enter the server IP: ");
+            serverIP = inFromUser.readLine();
+            if (isValidIP(serverIP)) {
+                break;
+            }
+            System.out.println("Invalid IP address. Please enter a valid IPv4 address.");
+        }
+
+        while (true) {
+            System.out.print(">> Enter the server port: ");
+            try {
+                serverPort = Integer.parseInt(inFromUser.readLine());
+                if (serverPort >= 0 && serverPort <= 65535) {
+                    break;
+                }
+                System.out.println("Invalid port number. Please enter a number between 0 and 65535.");
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid port number. Please enter a number between 0 and 65535.");
+            }
+        }
+
+        int attempts = 0;
+        while (attempts < 4) {
+            try {
+                clientSocket = new Socket(serverIP, serverPort);
+                break; // Connection successful, exit loop
+            } catch (IOException e) {
+                attempts++;
+                System.out.println("Failed to connect. Attempt " + attempts + " of 4.");
+                if (attempts < 4) {
+                    Thread.sleep(1000); // Wait for 1 second before next attempt
+                }
+            }
+        }
+
+        if (clientSocket == null) {
+            System.out.println("Could not connect to the server after 4 attempts. Exiting.");
+            return;
+        }
+
+        System.out.println("Connecting to server...");
+        System.out.println("Type exit or quit at any time to disconnect.");
 
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 
-        System.out.println("Connecting to server...");
-        System.out.println("Type exit or quit at any time to disconect.");
-
         while (true) {
             response = inFromServer.readLine();
             if (response == null) {
+                System.out.println("Server disconnected.");
                 break; // Server disconnected
             }
             
@@ -36,7 +91,6 @@ public class Main {
                 System.out.println(response);
             }
         }
-
         clientSocket.close();
     }
 }
